7d4a4ebef461bc5d327bc8fa11df9601b13698d8
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-09 18:24:05 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,33 @@
 // Mobile Collapse for Navbar
-
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-  });
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        edge: "right"
+    });
+});
 
 // Collapsible tasks
-
-$(document).ready(function(){
+$(document).ready(function () {
     $('.collapsible').collapsible();
-  });
+});
 
 // Tooltip
+$(document).ready(function () {
+    $('.tooltipped').tooltip();
+});
 
-$(document).ready(function(){
-$('.tooltipped').tooltip();
+/* Date Picker for Task Due Date
+  - format = the way the date is formatted to be stored in the database
+  - yearRange = the number of years to display on either side of the current year
+  - showClearBtn = gives the users the option to clear the datepicker
+  - i18n = allows us to customise the calendar text - changing done button to select
+*/
+$(document).ready(function () {
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "select"
+        }
+    });
 });
