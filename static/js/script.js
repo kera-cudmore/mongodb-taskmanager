1b5549c9f816927b942bd88f623dcfa6080ff2d8
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-09 08:33:26 +0000

    add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
   });
+
+// Collapsible tasks
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
